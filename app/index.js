96adaad31bc4cb689d16cda49bf3c4a765698ddb
sigsegv <sigsegv@quantumfish.com>
2017-03-22 17:35:32 -0700

    update hot loading support

--- app/index.js
+++ app/index.js
@@ -1,22 +1,31 @@
-// Import React and React DOM
 import * as React from 'react';
 import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 import App from './containers/App';
 import accountStore from './stores/Account';
 import { default as AccountTransport } from './transports/ipc/Account';
 // import './scss/general.scss';
 
-// [FIXME] need to fetch account state from backend
 const accountTransport = new AccountTransport();
 accountStore.setTransport(accountTransport);
 accountStore.getState();
-
-// Get the DOM Element that will host our React application.
-const rootEl = document.getElementById('root');
+// [FIXME] - pass store to app container
 
 render(
-  <div>
+  <AppContainer>
     <App />
-  </div>,
-  rootEl
+  </AppContainer>,
+   document.getElementById('root')
 );
+
+if (module.hot) {
+  module.hot.accept('./containers/App', () => {
+    const NextApp = require('./containers/App'); // eslint-disable-line global-require
+    render(
+      <AppContainer>
+        <NextApp />
+      </AppContainer>,
+      document.getElementById('root')
+    );
+  });
+}
