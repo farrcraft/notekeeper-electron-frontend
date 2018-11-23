2f1d7f60daff5add6e3b29542792c5a73cd40f40
Josh Farr <sigsegv@quantumfish.com>
2018-11-22 22:35:12 -0800

    update basic workspace to new material-ui

--- app/components/screens/BasicWorkspace/BasicWorkspace.js
+++ app/components/screens/BasicWorkspace/BasicWorkspace.js
@@ -1,39 +1,138 @@
 import React, { Component } from 'react';
 import { observer, inject, PropTypes } from 'mobx-react';
-import Button from '@material-ui/core/Button';
-import { Toolbar, ToolbarGroup } from '@material-ui/core/Toolbar';
+
+import IconButton from '@material-ui/core/IconButton';
+import Toolbar from '@material-ui/core/Toolbar';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+
+import { withStyles } from '@material-ui/styles';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+
+import HttpsIcon from '@material-ui/icons/Https';
+import NoteAddIcon from '@material-ui/icons/NoteAdd';
+
 import SplitPane from 'react-split-pane';
+
 import NoteView from '../../views/NoteView';
 import NotebookList from '../../views/NotebookList';
 import NoteList from '../../views/NoteList';
 import NotebookTitleModal from '../../modals/NotebookTitle';
 
+const styles = theme => ({
+  root: {
+    width: '100%'
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block'
+    }
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    marginRight: theme.spacing.unit * 2,
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing.unit * 3,
+      width: 'auto'
+    }
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%'
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: 200
+    }
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex'
+    }
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none'
+    }
+  }
+});
+
 @inject('account', 'notebook', 'note')
 @observer
 class BasicWorkspace extends Component {
-  handleLockAccount() {
+  handleLockAccount = () => {
     const { account } = this.props;
     account.lock();
-  }
+  };
 
-  handleNewNote() {
+  handleNewNote = () => {
     const { note } = this.props;
     note.create();
-  }
+  };
 
   render() {
+    const { classes } = this.props;
+
     return (
-      <div className="workspace">
-        <Toolbar>
-          <ToolbarGroup firstChild>
-            <Button label="New Note" onClick={() => this.handleNewNote()} />
-            <Button
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              label="New Note"
+              onClick={this.handleNewNote}
+            >
+              <NoteAddIcon />
+            </IconButton>
+            <IconButton
+              color="inherit"
               label="Lock Account"
-              onClick={() => this.handleLockAccount()}
-            />
-          </ToolbarGroup>
-        </Toolbar>
-        <h1>Workspace</h1>
+              onClick={this.handleLockAccount}
+            >
+              <HttpsIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Typography
+          className={classes.title}
+          variant="h1"
+          color="inherit"
+          noWrap
+        >
+          Workspace
+        </Typography>
         <SplitPane split="vertical">
           <NotebookList />
           <SplitPane split="horizontal">
@@ -47,9 +146,10 @@ class BasicWorkspace extends Component {
   }
 }
 
-BasicWorkspace.propTypes = {
+BasicWorkspace.wrappedComponent.propTypes = {
   account: PropTypes.observableObject.isRequired,
-  note: PropTypes.observableObject.isRequired
+  note: PropTypes.objectOrObservableObject.isRequired,
+  classes: PropTypes.objectOrObservableObject.isRequired
 };
 
-export default BasicWorkspace;
+export default withStyles(styles)(BasicWorkspace);
